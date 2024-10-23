41e47075368911b0232ceac9b290a1286b9832d0
allendhere <bolivarvjd@gmail.com>
2024-10-23 13:08:49 -0500

    segundo trabajo del profe

---
+++ flask/v4_login/main.py
@@ -0,0 +1,33 @@
+#importar flask
+from flask import Flask, render_template
+
+#declarar variable app
+app=Flask(__name__)
+#crear funcion de inicio y rutas
+
+
+@app.route('/')
+def index():
+    return render_template('principal.html')
+
+@app.route('/ENLACE_1')
+def Enlace_1():
+    return render_template('pagina_1.html')
+
+@app.route('/ENLACE_2')
+def Enlace_2():
+    return render_template('pagina_2.html')
+
+@app.route('/LOGIN')
+def Login():
+    return render_template('login.html')
+
+@app.route('/APLICACION')
+def Aplicacion():
+    return render_template('aplicacion.html')
+
+
+
+#fin cuerpo 
+if __name__ == '__main__':
+    app.run(debug=True)
