d4d4cf69d9dd74ad12c5624f2c1500cbadbe91dd
allendhere <bolivarvjd@gmail.com>
2024-11-10 12:14:42 -0500

    ordene las paginas en el proyecto principal

--- flask/v4_login/main.py
+++ flask/v4_login/main.py
@@ -7,28 +7,28 @@ app=Flask(__name__)
 
 @app.route('/LOGIN')
 def Login():
-    return render_template('login.html')
+    return render_template('formularios/login.html')
 
 @app.route('/')
 def index():
-    return render_template('principal.html')
+    return render_template('pagina/principal.html')
 
 @app.route('/ENLACE_1')
 def Enlace_1():
-    return render_template('pagina_1.html')
+    return render_template('pagina/pagina_1.html')
 
 @app.route('/ENLACE_2')
 def Enlace_2():
-    return render_template('pagina_2.html')
+    return render_template('pagina/pagina_2.html')
 
 @app.route('/APLICACION')
 def Aplicacion():
-    return render_template('aplicacion.html')
+    return render_template('formularios/aplicacion.html')
 
 
 @app.route('/DBPERSONAJES')
 def DBPersonajes():
-    return render_template('DBPersonajes.html')
+    return render_template('pagina/DBPersonajes.html')
 
 
 
